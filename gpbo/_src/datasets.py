e91faf955ba8555bc2aa6428f9efa83f958e5098
cemlyn007 <cemlynwaters@gmail.com>
2024-01-12 21:52:31 +0000

    feat: add `mean_center_dataset`

--- gpbo/_src/datasets.py
+++ gpbo/_src/datasets.py
@@ -38,3 +38,13 @@ def min_max_scale_dataset(
     xs, xs_min, xs_max = min_max_scale(dataset.xs)
     ys, ys_min, ys_max = min_max_scale(dataset.ys)
     return Dataset(xs, ys), Dataset(xs_min, ys_min), Dataset(xs_max, ys_max)
+
+
+def mean_center_dataset(
+    dataset: Dataset,
+) -> tuple[Dataset, Dataset]:
+    xs_mean = jnp.mean(dataset.xs, axis=0)
+    ys_mean = jnp.mean(dataset.ys, axis=0)
+    return Dataset(dataset.xs - xs_mean, dataset.ys - ys_mean), Dataset(
+        xs_mean, ys_mean
+    )

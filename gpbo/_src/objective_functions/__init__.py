78094976d6f98cfa5b54413a5af0192bc11dedf1
Cemlyn <cemlynwaters@gmail.com>
2024-01-21 09:41:54 +0000

    feat: export `ObjectiveFunction` and `Boundary`

--- gpbo/_src/objective_functions/__init__.py
+++ gpbo/_src/objective_functions/__init__.py
@@ -1,3 +1,4 @@
+from gpbo._src.objective_functions.core import ObjectiveFunction, Boundary
 from gpbo._src.objective_functions.mnist import MnistObjectiveFunction
 from gpbo._src.objective_functions.six_hump_camel import SixHumpCamelObjectiveFunction
 from gpbo._src.objective_functions.univariate import UnivariateObjectiveFunction

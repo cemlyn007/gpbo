c666ae49a75708ec61b1a61e2295813293e767dc
cemlyn007 <cemlynwaters@gmail.com>
2024-03-04 00:01:03 +0000

    docs: export 3d to vtk script

---
+++ examples/export_3d_to_vtk.py
@@ -0,0 +1,56 @@
+import pyvista
+import numpy as np
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser("Export 3D data to VTK")
+    parser.add_argument(
+        "--ticks_filepath",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--grid_ys_filepath",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--mean_filepath",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--std_filepath",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--utility_filepath",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--save_filepath",
+        type=str,
+        default=None,
+    )
+
+    arguments = parser.parse_args()
+
+    ticks = np.load(arguments.ticks_filepath)
+    grid_ys = np.load(arguments.grid_ys_filepath)
+
+    structured_grid = pyvista.StructuredGrid(*np.meshgrid(*ticks))
+    structured_grid["ys"] = grid_ys.ravel('F')
+    if arguments.mean_filepath:
+        mean = np.load(arguments.mean_filepath)
+        structured_grid["mean"] = mean.ravel('F')
+    if arguments.std_filepath:
+        std = np.load(arguments.std_filepath)
+        structured_grid["std"] = std.ravel('F')
+    if arguments.utility_filepath:
+        utility = np.load(arguments.utility_filepath)
+        structured_grid["utility"] = utility.ravel('F')
+
+    structured_grid.save(arguments.save_filepath)
